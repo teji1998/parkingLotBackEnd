7cfec7311a11572c194e4b598a3458f41d36c220
“teji1998” <tejasvinirpk@gmail.com>
2020-11-06 10:49:30 +0530

    Refactored:Code to send a mail while registration

--- src/main/java/com/bridgelabz/spring/parkinglot/service/UserService.java
+++ src/main/java/com/bridgelabz/spring/parkinglot/service/UserService.java
@@ -5,6 +5,9 @@ import com.bridgelabz.spring.parkinglot.exception.UserException;
 import com.bridgelabz.spring.parkinglot.model.UserDetails;
 import com.bridgelabz.spring.parkinglot.repository.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,6 +16,20 @@ public class UserService {
     @Autowired
     IUserRepository userRepository;
 
+    @Autowired
+    JavaMailSender javaMailSender;
+
+
+    public void sendSimpleMessage(UserDTO userDTO) throws MailException {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("tejasvinirpk@gmail.com");
+        message.setTo(userDTO.getEmailId());
+        message.setSubject("Registration message");
+        message.setText("Your registration is done successfully");
+        javaMailSender.send(message);
+    }
+
+
     public String registerUser(UserDTO userDTO) {
         if (userRepository.findByEmail(userDTO.getEmailId()).isPresent())
             return "Email already exists";
@@ -20,6 +37,7 @@ public class UserService {
         user.setEmail(userDTO.getEmailId());
         user.setPassword(userDTO.getPassword());
         userRepository.save(user);
+        sendSimpleMessage(userDTO);
         return "Registration done";
     }
 
